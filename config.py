422aee48d371c08da1b8f406d85c88dd46f19cdf
LocationEarth <hommersklich@gmail.com>
2025-06-20 18:21:11 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+import os
+
+# Es wird dringend empfohlen, den API-Schlüssel als Umgebungsvariable zu setzen
+# und nicht direkt in den Code zu schreiben.
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
